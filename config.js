86f0bc0a144aa893ea0787f62b25dcef57936c37
dcwestra <104077563+dcwestra@users.noreply.github.com>
2026-08-05 15:20:33 -0400

    Update MagicMirror² configuration with new modules
    Remove deprecated modules and add replacements

--- config.js
+++ config.js
@@ -38,13 +38,23 @@ let config = {
         // true, force serveronly mode, because you want to.. no UI on this device
 	
 modules: [
+{
+            module: "alert",
+    },
     {
-		module: "alert",
-	},
-	{
-		module: "clock",
-		position: "top_right"
-	},
+        module: "clock",
+        position: "top_right",
+        config: {
+                 showDate: false,
+                },
+    },
+    {
+        module: "clock",
+        position: "top_left",
+        config: {
+                 showTime: false,
+                },
+    },
 	{
 		module: 'MMM-CalendarExt2',
 		config: {
@@ -58,16 +68,16 @@ modules: [
 				url: "https://www.google.com/calendar/ical/en.usa%23holiday@group.v.calendar.google.com/public/basic.ics",
 			},
 			],
-		views: [
-			{
-				name: "view1",
-				mode: "month",
-				maxItems: 1000,
-				hideOverflow: false,
-				slotMaxHeight: 100px, //100px works great for months that show 5 weeks on the screen - use 90px when a month flows over into a 6th week
-				monthFormat: "MMMM YYYY",
-				timeFormat: "h:mm A",
-				position: "top_left",
+        views: [
+            {
+                name: "view1",
+                mode: "week",
+                slotCount: 4,
+                slotMaxHeight: "110px",
+                monthFormat: "MMMM",
+                timeFormat: "h:mm A",
+                position: "top_left",
+                weekStart: 0,
 			},
 			],
 		scenes: [
@@ -132,18 +142,41 @@ modules: [
             ignoreOlderThan: 604800000
         }
     },
+      {
+        module: 'MMM-CulversFOTD',
+        position: 'top_right',  // or wherever you want to display it
+        config: {
+            storeSlug: 'sauk-city',
+            updateInterval: 3600000 // optional: set how often the flavor should update
+        }
+    },
     {
-		module: 'MMM-MicrosoftToDo',
-		position: 'top_right',      // This can be any of the regions. Best results in left or right regions.
-		header: 'groceries', // This is optional
-		config: {
-			oauth2ClientSecret: 'YourSecretID',
-			oauth2RefreshToken: 'yourRefreshToken',
-			oauth2ClientId: 'yourClientID',
-			listName: 'groceries', // optional parameter: if not specified displays tasks from default "Tasks" list, if specified will look for a task list with the specified name (exact spelling), don't specify if you want to make use of the>      refreshSeconds: 60, // optional parameter: every how many seconds should the list be updated from the remote service, default value is 60
-		}
-	},
-        ]
+            module: "newsfeed",
+            position: "bottom_bar",
+            config: {
+                    feeds: [
+                    {
+                            title: "New York Times",
+                            url: "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+                    },
+                    {
+                            title: "NPR",
+                            url: "https://feeds.npr.org/1001/rss.xml"
+                    },
+                    {
+                            title: "WOOD TV 8",
+                            url: "https://www.woodtv.com/feed/"
+                    },
+                    ],
+                    showSourceTitle: true,
+                    showPublishDate: true,
+                    broadcastNewsFeeds: true,
+                    broadcastNewsUpdates: true,
+                    ignoreOldItems: true,
+                    ignoreOlderThan: 604800000
+            }
+    },
+]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
